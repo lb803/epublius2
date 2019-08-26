4945a8d0c6de066f7455a23bd27a7efbeb27f5bf
Luca Baffa <lb803@mailbox.org>
2019-08-26 09:55:47 +0100

    added pandoc template and modules/pandoc.py module.

---
+++ modules/pandoc.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+import tempfile
+import datetime
+import html
+
+class Pandoc():
+    def __init__(self, config):
+        self.config = config
+        
+    def convert_files(self, content, metadata):
+            
+        cmd = ['pandoc', '-s',
+               '--template', os.path.abspath(self.config['template']),
+               '--metadata-file={}'.format(metadata),
+               content['input_path'],
+               '-o', content['output_path'],
+               '--to=html5']
+
+        try:
+            p = subprocess.Popen(cmd,
+                                 stdout = subprocess.PIPE,
+                                 stderr = subprocess.PIPE)
+            stdout, stderr = p.communicate()
+
+            assert p.returncode == 0
+            
+        except AssertionError:
+            print('[ERROR] Standard error of subprocess: {}' \
+                  .format(stderr))
+            raise
+
